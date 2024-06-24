c374807eda265963ddca902b42f15edb3fba1846
JinSooo <kimjinso@qq.com>
2024-06-24 09:10:28 +0800

    feat(vue): ✨ computed

--- packages/vue/vue-design-implementation/reactivity/index.ts
+++ packages/vue/vue-design-implementation/reactivity/index.ts
@@ -1,10 +1,14 @@
 interface EffectOption {
-  scheduler?(effectFn: EffectFn): void
+  lazy?: boolean
+
+  scheduler?(effectFn: Effect): void
 }
 
-interface EffectFn {
-  (): void
-  deps?: Array<Set<EffectFn>>
+type EffectFn = () => any
+
+interface Effect {
+  (): any
+  deps?: Array<Set<Effect>>
   options?: EffectOption
 }
 
@@ -29,7 +33,7 @@ type Target = object
 const bucket = new WeakMap<Target, Map<Key, Set<EffectFn>>>()
 
 // 清除副作用函数依赖
-const cleanup = (effectFn: EffectFn) => {
+const cleanup = (effectFn: Effect) => {
   effectFn.deps!.forEach(deps => {
     deps.delete(effectFn)
   })
@@ -37,7 +41,7 @@ const cleanup = (effectFn: EffectFn) => {
 }
 
 // 全局变量存储被注册的副作用函数
-let activeEffect: EffectFn
+let activeEffect: Effect
 // effect 栈，用来存储嵌套 effectFn
 const effectStack: Array<EffectFn> = []
 const effect = (fn: EffectFn, options: EffectOption = {}) => {
@@ -47,17 +51,58 @@ const effect = (fn: EffectFn, options: EffectOption = {}) => {
     // 在调用副作用函数前，将当前副作用函数压入栈中
     effectStack.push(effectFn)
     // 执行函数以被Proxy拦截，进行追踪操作
-    fn()
+    const res = fn()
     // 在函数执行完毕后，将当前副作用函数弹出栈
     effectStack.pop()
     // 恢复之前的 effectFn 值
     activeEffect = effectStack[effectStack.length - 1]
+    return res
   }
 
   effectFn.options = options
   // 用来存储所有与该副作用函数相关联的依赖集合
   effectFn.deps = []
-  effectFn()
+  // 懒加载
+  if (!options.lazy) {
+    effectFn()
+  }
+  // 返回副作用函数供自行调用
+  return effectFn
+}
+
+// 计算属性
+const computed = (getter: EffectFn) => {
+  // value 用来缓存上一次计算的值
+  let value: any
+  // dirty 用来标识是否需要重新计算
+  let dirty = true
+
+  const effectFn = effect(getter, {
+    lazy: true,
+    scheduler(effectFn) {
+      dirty = true
+      // 手动调用触发响应
+      trigger(computedObj, 'value')
+      effectFn()
+    },
+  })
+
+  // 对 computedObj 进行响应式处理
+  const computedObj = {
+    get value() {
+      if (dirty) {
+        value = effectFn()
+        dirty = false
+      }
+
+      // 手动进行追踪
+      track(computedObj, 'value')
+
+      return value
+    },
+  }
+
+  return computedObj
 }
 
 /**
@@ -91,7 +136,7 @@ const trigger = (target: Target, key: Key) => {
   if (!depsMap) return true
   const effects = depsMap.get(key)
 
-  const effectsToRun = new Set<EffectFn>()
+  const effectsToRun = new Set<Effect>()
   effects?.forEach(effectFn => {
     // 如果 trigger 触发的 effectFn 与当前正在执行的副作用函数相同，则不触发执行
     if (effectFn !== activeEffect) {
@@ -128,20 +173,11 @@ const obj = new Proxy(data, {
   },
 })
 
-effect(
-  () => {
-    console.log('effectFn1 run')
+const sum = computed(() => (obj.ok ? obj.text.length : 0))
 
-    obj.count
-  },
-  {
-    scheduler(effectFn) {
-      setTimeout(() => {
-        effectFn()
-      }, 1000)
-    },
-  },
-)
+effect(() => console.log(`+${sum.value}`))
 
-obj.count++
+console.log(sum.value)
+obj.ok = false
+console.log(sum.value)
 console.log('finished')
