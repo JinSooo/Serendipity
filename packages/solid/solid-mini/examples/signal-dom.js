34b8232dfe24ebab14620a61b7d820ad0fa8b783
JinSo <kimjinso@qq.com>
2024-08-14 18:54:12 +0800

    feat(solid): ✨ solid-mini add memo example

--- packages/solid/solid-mini/examples/signal-dom.js
+++ packages/solid/solid-mini/examples/signal-dom.js
@@ -1,11 +1,19 @@
-import { createSignal, createEffect } from '../packages/solid/src/reactive/dist/signal.js'
+import { createSignal, createEffect, createMemo } from '../packages/solid/src/reactive/dist/signal.js'
 
 const signal1Elem = document.getElementById('signal1')
 const signal2Elem = document.getElementById('signal2')
+const memo1Elem = document.getElementById('memo1')
 const triggerElem = document.getElementById('trigger')
 
 const [signal1, trigger1] = createSignal(1)
 const [signal2, trigger2] = createSignal('1')
+const memo1 = createMemo(() => {
+  console.log('memo1', signal1() * 2)
+
+  memo1Elem.textContent = signal1() * 2
+
+  return signal1() * 2
+})
 
 // computation1
 createEffect(() => {
